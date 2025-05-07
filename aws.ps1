bc0f98a89a85c74b3399f0103d5173b898b86445
JamesSowell <145jimmi@gmail.com>
2025-05-06 20:44:59 -0700

    added colored function to list events of specified stack

--- aws.ps1
+++ aws.ps1
@@ -1,13 +1,10 @@
-function dubstacks {
+function dstacks {
     param (
         [string]$keyword
     )
     aws cloudformation list-stacks --query "StackSummaries[?contains(StackName,'$keyword')].[StackName, StackStatus]"
 }
 
-function dubstack {
-
-}
 
 function trailRole {
 
@@ -48,6 +45,71 @@ function dstackgetname {
     Write-Host "Stack Name: $stackName"
     Write-Host "Stack Status: $stackStatus"
     
+}
+
+
+function dstackevent {
+    param(
+        [string]$stackName
+    )
+
+    # define colors
+    $red = [System.ConsoleColor]::Red
+    $green = [System.ConsoleColor]::Green
+    $yellow = [System.ConsoleColor]::Yellow
+    $blue = [System.ConsoleColor]::Blue
+    $orange = [System.ConsoleColor]::Orange
+    $default = [System.ConsoleColor]::White
+
+    # Get stack events
+    $events = aws cloudformation describe-stack-events --stack-name $stackName | ConvertFrom-Json
+    # Function to set color
+    function Set-Color {
+        param(
+            [System.ConsoleColor]$color
+        )
+        [System.Console]::ForegroundColor = $color
+    }
+
+    # iterate through events and print with color
+    foreach ($event in $events.StackEvents) {
+        $resourceStatus = $event.ResourceStatus
+        $resouceType = $event.ResourceType
+
+        # Set color based on resourceStatus
+        switch( $resource){
+            "ROLLBACK_COMPLETE" { Set-Color $red }
+            "ROLLBACK_FAILED" { Set-Color $red }
+            "CREATE_COMPLETE" { Set-Color $green }
+            "UPDATE_COMPLETE" { Set-Color $green }
+            "CREATE_IN_PROGRESS" { Set-Color $yellow }
+            "UPDATE_IN_PROGRESS" { Set-Color $yellow }
+            "DELETE_COMPLETE" { Set-Color $green }
+            "DELETE_IN_PROGRESS" { Set-Color $yellow }
+            "ROLLBACK_IN_PROGRESS" { Set-Color $yellow }
+            "CREATE_FAILED" { Set-Color $red }
+            "REVIEW_IN_PROGRESS" { Set-Color $orange }
+            default { Set-Color $default }
+        }
+
+
+        # print ResoruceStatus
+        Write-Host "ResouceStatus: $resourceStatus"
+
+        # set color for resourceType
+        Set-Color $blue
+        Write-Host "ResouceType: $resourceType"
+
+        # reset color to deault
+        Set-Color $default
+        Write-Host "Timestamp: $($event.Timestamp)"
+        Write-Host "LogicalResourceId: $($event.LogicalResourceId)"
+        Write-Host "PhysicalResourceId: $($event.PhysicalResourceId)"
+        Write-Host "---------------------------------------------"
+    }
+
+    # Reset color to default at the end
+    Set-Color $default
 }
 
 function dublogin {
